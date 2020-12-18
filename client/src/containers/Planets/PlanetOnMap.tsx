e5b6e5c2c2f4050af2905fe4889a461fbc0634c3
Иван <if1242@yandex.ru>
2020-12-18 03:32:26 +0300

    Update PlanetOnMap.tsx

--- client/src/containers/Planets/PlanetOnMap.tsx
+++ client/src/containers/Planets/PlanetOnMap.tsx
@@ -11,14 +11,16 @@ export interface PlanetOnMapProps {
   onClick: () => void;
 }
 
+
 export function PlanetOnMap({
   data,
   onClick,
 }: PlanetOnMapProps): React.ReactElement {
+  const planetName = data.name === "DION ZJ97" ? "dion" : data.name.toLowerCase();
   return (
-    <div className={`spacemap-${data.name}`}>
+    <div className={`spacemap-${planetName}`}>
       <div>
-        <img src={`./img/spacemap-${data.name}.png`} onClick={onClick} />
+        <img src={`./img/spacemap-${planetName}.png`} onClick={onClick} />
         <br />
         <h6 className={"spacemap-planet-name"}>{data.name}</h6>
       </div>
